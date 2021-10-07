616ce306d25bf6035c1fb9e862f51a72a27f9fce
KarmandAziz <Karmand94@gmail.com>
2021-10-07 09:52:21 +0200

    Initialized Project

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * guessing game
+ * @author Karmand Aziz
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
